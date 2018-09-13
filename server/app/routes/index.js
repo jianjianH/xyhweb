d4adef59d5ca7955ae7ef7fdbaa97dcff2902de0
坚坚老师 <jnehuang@gmail.com>
2018-09-13 21:43:18 +0800

    删除无效信息

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,18 +1,10 @@
-module.exports =  (router) => {
-  router.get('/welcome', async function (ctx, next) {
-    ctx.state = {
-      title: 'koa2 title'
-    };
-
-    await ctx.render('index', {title: ctx.state});
-  });
-
+module.exports = (router) => {
   router.get('/test', async function (ctx, next) {
     ctx.state = {
       result: 1,
       data: 'this is test data'
     };
 
-    await ctx.render('json', {result: ctx.state.result, data: ctx.state.data});
+    await ctx.render('json', { jsondata: JSON.stringify(ctx.state) });
   })
 }
