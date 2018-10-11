9893f0117e19edf0c75487f485ca1cd9e114d718
Jne <jnehuang@welove-inc.com>
2018-10-11 18:32:18 +0800

    add datas

--- server/app/utils/log_util.js
+++ server/app/utils/log_util.js
@@ -32,77 +32,78 @@ logUtil.logInfo = function (info) {
 
 let formatInfo = function (info) {
     let logText = new String();
-    //响应日志开始
+    // 响应日志开始
     logText += "\n" + "***************info log start ***************" + "\n";
 
-    //响应内容
+    // 响应内容
     logText += "info detail: " + "\n" + JSON.stringify(info) + "\n";
 
-    //响应日志结束
+    // 响应日志结束
     logText += "*************** info log end ***************" + "\n";
 
     return logText;
 }
 
-//格式化响应日志
+// 格式化响应日志
 let formatRes = function (ctx, resTime) {
     let logText = new String();
-    //响应日志开始
+    // 响应日志开始
     logText += "\n" + "*************** response log start ***************" + "\n";
 
-    //添加请求日志
+    // 添加请求日志
     logText += formatReqLog(ctx.request, resTime);
 
-    //响应状态码
+    // 响应状态码
     logText += "response status: " + ctx.status + "\n";
 
-    //响应内容
-    logText += "response body: " + "\n" + JSON.stringify(ctx.body) + "\n";
+    // 响应内容
+    // 只在测试环境打印响应内容，正式环境log太多了
+    if(__dirname.indexOf('test') > 0) logText += "response body: " + "\n" + JSON.stringify(ctx.body) + "\n";
 
-    //响应日志结束
+    // 响应日志结束
     logText += "*************** response log end ***************" + "\n";
 
     return logText;
 }
 
-//格式化错误日志
+// 格式化错误日志
 let formatError = function (ctx, err, resTime) {
     let logText = new String();
 
-    //错误信息开始
+    // 错误信息开始
     logText += "\n" + "*************** error log start ***************" + "\n";
 
-    //添加请求日志
+    // 添加请求日志
     logText += formatReqLog(ctx.request, resTime || 0);
 
-    //错误名称
+    // 错误名称
     logText += "err name: " + err.name + "\n";
-    //错误信息
+    // 错误信息
     logText += "err message: " + err.message + "\n";
-    //错误详情
+    // 错误详情
     logText += "err stack: " + err.stack + "\n";
 
-    //错误信息结束
+    // 错误信息结束
     logText += "*************** error log end ***************" + "\n";
 
     return logText;
 };
 
-//格式化请求日志
+// 格式化请求日志
 let formatReqLog = function (req, resTime) {
     let logText = new String();
 
     let method = req.method;
-    //访问方法
+    // 访问方法
     logText += "request method: " + method + "\n";
 
-    //请求原始地址
+    // 请求原始地址
     logText += "request originalUrl:  " + req.originalUrl + "\n";
 
-    //客户端ip
+    // 客户端ip
     logText += "request client ip:  " + req.ip + "\n";
 
-    //请求参数
+    // 请求参数
     if (method === 'GET') {
         logText += "request query:  " + JSON.stringify(req.query) + "\n";
         // startTime = req.query.requestStartTime;
@@ -110,7 +111,7 @@ let formatReqLog = function (req, resTime) {
         logText += "request body: " + "\n" + JSON.stringify(req.body) + "\n";
         // startTime = req.body.requestStartTime;
     }
-    //服务器响应时间
+    // 服务器响应时间
     logText += "response time: " + (resTime || 0) + "\n";
 
     return logText;
