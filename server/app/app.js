c36309abec3c036fc943aa27ed05ce89caa7e519
坚坚老师 <jnehuang@gmail.com>
2018-12-30 18:24:15 +0800

    格式化代码

--- server/app/app.js
+++ server/app/app.js
@@ -56,7 +56,7 @@ app
   }))
   .use(router.routes())
 
-app.on('error', function (err, ctx) {
+app.on('error', (err, ctx) => {
   console.log(err)
   logger.error('server error', err, ctx)
 })
--- server/app/config/log_init.js
+++ server/app/config/log_init.js
@@ -4,7 +4,7 @@ const log_config = require('./log_config');
 /**
  * 确定目录是否存在，如果不存在则创建目录
  */
-let confirmPath = function (pathStr) {
+let _confirmPath = (pathStr) => {
     if (!fs.existsSync(pathStr)) {
         fs.mkdirSync(pathStr);
         console.log('createPath: ' + pathStr);
@@ -14,14 +14,14 @@ let confirmPath = function (pathStr) {
 /**
  * 初始化log相关目录
  */
-let initLogPath = function () {
+let initLogPath = () => {
     // 创建log的根目录'logs'
     if (log_config.baseLogPath) {
-        confirmPath(log_config.baseLogPath)
+        _confirmPath(log_config.baseLogPath);
         // 根据不同的logType创建不同的文件目录
         for (let i = 0, len = log_config.appenders.length; i < len; i++) {
             if (log_config.appenders[i].path) {
-                confirmPath(log_config.baseLogPath + log_config.appenders[i].path);
+                _confirmPath(log_config.baseLogPath + log_config.appenders[i].path);
             }
         }
     }
