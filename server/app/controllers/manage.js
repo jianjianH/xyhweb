762fa1e0386c8496a664f8acf20f138229d70abe
jiangzixu <619999372@qq.com>
2019-03-09 14:22:50 +0800

    增加添加banner方法

--- server/app/controllers/manage.js
+++ server/app/controllers/manage.js
@@ -6,11 +6,29 @@
   * 新建Banner(添加一条新的banner)
   */
  const db = require("../utils/db");
+ const moment = require('moment')
  let addBanner = async (ctx,next)=>{
      // 请求参数
-    const query = ctx.request.query;
-    console.log(query);
+    let data = ctx.request.body;
+    let now = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+    data['create_time'] =  now;
+    data['update_time'] =  now;
+    console.log(data);
+    // 返回参数(暂时不清楚mysql返回自增主键的值是怎么样的)
+    let returnBody = {
+        "result": 1,
+        "data": {
+            'banner_id':123
+        }
+    }
+   db.insert(data,'banner',(result)=>{
+       console.log(result)
+   });
+   console.log(result)
+      //返回给前端banner_id
+      ctx.state = returnBody;
  }
+ 
 
  module.exports = {
      addBanner
